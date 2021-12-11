c4dd18550e55e0e85db116fd48b89c65b5266952
diazdaiz <muhammaddiazmahaputra@yahoo.co.id>
2021-12-11 16:59:09 +0700

    solution

--- Problem_Solving/Assets/Scripts/Ball.cs
+++ Problem_Solving/Assets/Scripts/Ball.cs
@@ -3,9 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour {
-    Vector2 initial_velocity;
-    void Start() {
-        initial_velocity = new Vector2(3f, 3f);
-        transform.GetComponent<Rigidbody2D>().velocity = initial_velocity;
+    //Vector2 initial_velocity;
+    Vector2 velocity;
+    //void Start() {
+    //    initial_velocity = new Vector2(3f, 3f);
+    //    transform.GetComponent<Rigidbody2D>().velocity = initial_velocity;
+    //}
+
+    void Update() {
+        velocity = new Vector2(0f, 0f);
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            velocity += new Vector2(0f, -3f);
+        }
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            velocity += new Vector2(0f, 3f);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            velocity += new Vector2(-3f, 0f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            velocity += new Vector2(3f, 0f);
+        }
+        velocity = velocity.normalized * 3f;
+        transform.GetComponent<Rigidbody2D>().velocity = velocity;
     }
 }
