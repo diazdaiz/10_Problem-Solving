904c633ef524ffeb3137128a3ccd92f507cd32b1
diazdaiz <muhammaddiazmahaputra@yahoo.co.id>
2021-12-14 13:40:10 +0700

    change for problem 10

---
+++ Problem_Solving/Assets/Scripts/GameManagerSeven.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerSeven : MonoBehaviour
+{
+    public GameObject square;
+    public float spawnInterval;
+    int maxSpawn;
+    List<GameObject> squares;
+
+    int score;
+    public Text scoreText;
+
+    private void Awake() {
+        score = 0;
+        maxSpawn = Random.Range(3, 14);
+        squares = new List<GameObject>();
+
+        for (int i = 0; i < maxSpawn; i++) {
+            squares.Add(Instantiate(square));
+            squares[i].transform.position = new Vector3(Random.Range(-8.3f, 8.3f), Random.Range(-4.3f, 4.3f), 0f);
+        }
+    }
+
+    public void AddScore() {
+        score += 1;
+        scoreText.text = score.ToString();
+    }
+}
