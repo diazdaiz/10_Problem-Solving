901ff0911865ad15942f843c2c0648930e051d50
diazdaiz <muhammaddiazmahaputra@yahoo.co.id>
2021-12-14 13:29:19 +0700

    change for problem 10

---
+++ Problem_Solving/Assets/Scripts/BallThree.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallThree : MonoBehaviour {
+    Vector2 initial_velocity;
+    void Start() {
+        initial_velocity = new Vector2(3f, 3f);
+        transform.GetComponent<Rigidbody2D>().velocity = initial_velocity;
+    }
+}
