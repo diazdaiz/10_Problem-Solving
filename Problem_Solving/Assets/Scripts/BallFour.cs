ddc2e6f7171e4b06ddfe37913d3d7a3d4193a3e1
diazdaiz <muhammaddiazmahaputra@yahoo.co.id>
2021-12-14 13:31:52 +0700

    change for problem 10

---
+++ Problem_Solving/Assets/Scripts/BallFour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallFour : MonoBehaviour {
+    Vector2 velocity;
+
+    void Update() {
+        velocity = new Vector2(0f, 0f);
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            velocity += new Vector2(0f, -3f);
+        }
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            velocity += new Vector2(0f, 3f);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            velocity += new Vector2(-3f, 0f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            velocity += new Vector2(3f, 0f);
+        }
+        velocity = velocity.normalized * 3f;
+        transform.GetComponent<Rigidbody2D>().velocity = velocity;
+    }
+}
