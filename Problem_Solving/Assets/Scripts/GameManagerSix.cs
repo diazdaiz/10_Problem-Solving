f44cb38c278a3143c255c28c73c7e8c8d7d0d416
diazdaiz <muhammaddiazmahaputra@yahoo.co.id>
2021-12-14 13:37:17 +0700

    change for problem 10

---
+++ Problem_Solving/Assets/Scripts/GameManagerSix.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerSix: MonoBehaviour
+{
+    public GameObject square;
+    public float spawnInterval;
+    int maxSpawn;
+    List<GameObject> squares;
+
+    private void Awake() {
+        maxSpawn = Random.Range(3, 14);
+        squares = new List<GameObject>();
+
+        for (int i = 0; i < maxSpawn; i++) {
+            squares.Add(Instantiate(square));
+            squares[i].transform.position = new Vector3(Random.Range(-8.3f, 8.3f), Random.Range(-4.3f, 4.3f), 0f);
+        }
+    }
+}
