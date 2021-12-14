4ec4238743053636a4db2962b0a929fe872b52c2
diazdaiz <muhammaddiazmahaputra@yahoo.co.id>
2021-12-14 12:01:37 +0700

    solution_10_step_1

---
+++ Problem_Solving/Assets/Scripts/MenuSelection.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSelection : MonoBehaviour {
+    public void loadScene(string scene) {
+        SceneManager.LoadScene(scene);
+    }
+}
