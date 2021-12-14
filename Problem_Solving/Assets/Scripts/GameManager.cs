79a7217b354651c4e255a2f1caffde23496b0fbe
diazdaiz <muhammaddiazmahaputra@yahoo.co.id>
2021-12-14 11:41:48 +0700

    solution

--- Problem_Solving/Assets/Scripts/GameManager.cs
+++ Problem_Solving/Assets/Scripts/GameManager.cs
@@ -5,53 +5,97 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject square;
+    public GameObject yellowSquare;
+    public GameObject blueSquare;
     public float spawnInterval;
-    public int maxSpawn;
-    List<GameObject> squares;
+    int maxSpawn;
+    List<GameObject> yellowSquares;
+    List<GameObject> blueSquares;
+    bool spawnBlue;
+    bool spawnYellow;
 
     int score;
     public Text scoreText;
 
-    public Transform ball; 
+    public Transform ball;
 
     float timer;
 
     private void Awake() {
         timer = 0;
         score = 0;
-        maxSpawn = Random.Range(10, 16);
-        squares = new List<GameObject>();
+        maxSpawn = 6;
+        yellowSquares = new List<GameObject>();
+        blueSquares = new List<GameObject>();
 
         for (int i = 0; i < maxSpawn; i++) {
-            squares.Add(Instantiate(square));
-            squares[i].transform.position = new Vector3(Random.Range(-8.3f, 8.3f), Random.Range(-4.3f, 4.3f), 0f);
+            yellowSquares.Add(Instantiate(yellowSquare));
+            yellowSquares[i].transform.position = new Vector3(Random.Range(-8.3f, 8.3f), Random.Range(-4.3f, 4.3f), 0f);
+            blueSquares.Add(Instantiate(blueSquare));
+            blueSquares[i].transform.position = new Vector3(Random.Range(-8.3f, 8.3f), Random.Range(-4.3f, 4.3f), 0f);
         }
+
+        spawnBlue = false;
+        spawnYellow = true;
     }
 
     public void AddScore() {
-        score += 1;
+        score += 2;
         scoreText.text = score.ToString();
     }
 
+    public void SubtractScore() {
+        score -= 1;
+        if (score < 0) {
+            score = 0;
+        }
+        scoreText.text = score.ToString();
+    }
+
+    public void ResetScore() {
+        score = 0;
+        scoreText.text = score.ToString();
+    }
 
     private void Update() {
         timer += Time.deltaTime;
-        if (timer > spawnInterval) {
+        if (timer > spawnInterval && spawnBlue == true) {
             timer = 0;
-            SpawnFromPool();
+            SpawnBlueFromPool();
+            spawnBlue = false;
+            spawnYellow = true;
+        }
+        if (timer > spawnInterval && spawnYellow == true) {
+            timer = 0;
+            SpawnYellowFromPool();
+            spawnYellow = false;
+            spawnBlue = true;
         }
     }
 
-    void SpawnFromPool() {
-        for (int i = 0; i < squares.Count; i++) {
-            if (!squares[i].activeInHierarchy) {
+    void SpawnBlueFromPool() {
+        for (int i = 0; i < blueSquares.Count; i++) {
+            if (!blueSquares[i].activeInHierarchy) {
                 Vector3 spawnLocation;
                 do {
                     spawnLocation = new Vector3(Random.Range(-8.3f, 8.3f), Random.Range(-4.3f, 4.3f), 0f);
                 } while (Mathf.Pow(Mathf.Pow(spawnLocation.x - ball.position.x, 2) + Mathf.Pow(spawnLocation.y - ball.position.y, 2), 1f / 2f) < 0.8);
-                squares[i].transform.position = spawnLocation;
-                squares[i].SetActive(true);
+                blueSquares[i].transform.position = spawnLocation;
+                blueSquares[i].SetActive(true);
+                break;
+            }
+        }
+    }
+
+    void SpawnYellowFromPool() {
+        for (int i = 0; i < yellowSquares.Count; i++) {
+            if (!yellowSquares[i].activeInHierarchy) {
+                Vector3 spawnLocation;
+                do {
+                    spawnLocation = new Vector3(Random.Range(-8.3f, 8.3f), Random.Range(-4.3f, 4.3f), 0f);
+                } while (Mathf.Pow(Mathf.Pow(spawnLocation.x - ball.position.x, 2) + Mathf.Pow(spawnLocation.y - ball.position.y, 2), 1f / 2f) < 0.8);
+                yellowSquares[i].transform.position = spawnLocation;
+                yellowSquares[i].SetActive(true);
                 break;
             }
         }
---
+++ Problem_Solving/Assets/Scripts/YellowBall.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowBall : MonoBehaviour
+{
+    Vector2 velocity;
+    Vector3 mousePosition;
+    public GameManager gameManager;
+
+    private void Update() {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mouseBallDifference = mousePosition - transform.position;
+        float magnitude = Mathf.Pow(Mathf.Pow(mouseBallDifference.x, 2f) + Mathf.Pow(mouseBallDifference.y, 2f), 1f / 2f);
+        if (Input.GetKey(KeyCode.Mouse1)) {
+            velocity = new Vector2(mouseBallDifference.x / magnitude, mouseBallDifference.y / magnitude) * -2f;
+            transform.GetComponent<Rigidbody2D>().velocity = velocity;
+        }
+        else {
+            if (magnitude > 0.05f) {
+                velocity = new Vector2(mouseBallDifference.x / magnitude, mouseBallDifference.y / magnitude) * 2f;
+            }
+            else {
+                velocity = new Vector2(0f, 0f);
+            }
+            transform.GetComponent<Rigidbody2D>().velocity = velocity;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "yellow square") {
+            collision.gameObject.SetActive(false);
+            gameManager.AddScore();
+        }
+        if (collision.gameObject.tag == "blue square") {
+            collision.gameObject.SetActive(false);
+            gameManager.SubtractScore();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag == "wall" || collision.gameObject.tag == "blue ball") {
+            gameManager.ResetScore();
+        }
+    }
+}
