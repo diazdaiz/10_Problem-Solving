db08853c8bada75e2f20530843b02e103adb25d7
diazdaiz <muhammaddiazmahaputra@yahoo.co.id>
2021-12-14 12:33:03 +0700

    change for problem 10

--- Problem_Solving/Assets/Scripts/BallTwo.cs
+++ Problem_Solving/Assets/Scripts/BallTwo.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallProblemTwo: MonoBehaviour {
+public class BallTwo: MonoBehaviour {
     public Vector2 velocity;
     void Start() {
         transform.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 2f);
